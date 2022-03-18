5fed17fc30588ebd2c26d3a7e5d934f7af70f608
Milena <68025219+milenaserrano@users.noreply.github.com>
2022-03-18 00:31:36 +0000

    exercicio novo

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask('app')
 
-@app.route('/') 
-def template():
-  return '<h1> Meu primeiro template </h1>'
+@app.route('/')
+def index():
+    return render_template('index.html',
+    )
 
 @app.route('/unifran') 
 def unifran():
   return '<h2>Universidade de Franca</h2>'
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
